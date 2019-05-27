6648049d06b7af25b784fe0adfaf847cba222966
Joseph Myalla <chanangae@gmail.com>
2019-05-27 20:06:59 +0300

    Updates

---
+++ precache-manifest.f89a525d2fe53c4b3907eb4696b44606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e71cc0261eda578d13",
+    "url": "/meterioreit/static/css/main.061c85dc.chunk.css"
+  },
+  {
+    "revision": "51e71cc0261eda578d13",
+    "url": "/meterioreit/static/js/main.51e71cc0.chunk.js"
+  },
+  {
+    "revision": "1c3f597827d265c452d8",
+    "url": "/meterioreit/static/js/runtime~main.1c3f5978.js"
+  },
+  {
+    "revision": "9c12b13286e6c6020471",
+    "url": "/meterioreit/static/js/2.9c12b132.chunk.js"
+  },
+  {
+    "revision": "b34aaba00e412cc57d4d798bfc805f4d",
+    "url": "/meterioreit/index.html"
+  }
+];
